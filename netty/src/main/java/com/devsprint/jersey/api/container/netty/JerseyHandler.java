9691f7e290468b9c43b4431d7ddbc7fcac887fc8
Gabriel Ciuloaica <gciuloaica@gmail.com>
2011-12-18 21:01:45 +0200

    changed visibility of classes and added license on top of the packages

--- netty/src/main/java/com/devsprint/jersey/api/container/netty/JerseyHandler.java
+++ netty/src/main/java/com/devsprint/jersey/api/container/netty/JerseyHandler.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2011 Gabriel Ciuloaica (gciuloaica@gmail.com)
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software
+ *distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and
+ *limitations under the License.
+ */
 package com.devsprint.jersey.api.container.netty;
 
 import java.io.IOException;
@@ -22,8 +37,7 @@ import com.sun.jersey.spi.container.WebApplication;
  * @author gciuloaica
  * 
  */
-public class JerseyHandler extends SimpleChannelUpstreamHandler {
-	public static final String PROPERTY_BASE_URI = "com.devsprint.learning.async_server.baseUri";
+class JerseyHandler extends SimpleChannelUpstreamHandler {
 
 	private final transient WebApplication application;
 	private final transient String baseUri;
@@ -32,7 +46,8 @@ public class JerseyHandler extends SimpleChannelUpstreamHandler {
 			final ResourceConfig resourceConfig) {
 		super();
 		this.application = application;
-		this.baseUri = (String) resourceConfig.getProperty(PROPERTY_BASE_URI);
+		this.baseUri = (String) resourceConfig
+				.getProperty(NettyServer.PROPERTY_BASE_URI);
 	}
 
 	@Override
